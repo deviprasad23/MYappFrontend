764744e30dfb829f15660fe4bf34a2fc513c0f81
Sujeeth Reddy Kasthuri <kasthurisujeethreddy@gmail.com>
2026-02-26 11:47:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Reg from './Reg';
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+    <h1>Iam App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/reg" element={<Reg />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+      </>
+  )
+}
+
+export default App;
---
+++ src/Reg.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+function Reg()
+{
+    const [data,setdata]=useState(
+        {
+            username:"",
+            email:"",
+            password:""
+        }
+    )
+    const changeName=(e)=>
+    {
+        setdata({...data,[e.target.name]:e.target.value})
+    }
+    const changeEmail=(e)=>
+    {
+        setdata({...data,[e.target.name]:e.target.value})
+    }
+    const changePassword=(e)=>
+    {
+        setdata({...data,[e.target.name]:e.target.value})
+    }
+    const submit=async()=>
+    {
+        try{
+            //const res=await axios.post("http://localhost:8080/reg",data)
+                const res=await axios.post("http://cabsystemsms-1.onrender.com/register",data)
+                alert(res.data)
+        }
+    catch(xyz)
+    {
+        //alert(xyz.response.data)
+        alert(xyz.response?.data || "Error")
+    }
+}
+return(
+    <>
+    <h1>iam App</h1>
+    <input onChange={changeName} name="username" placeholder="enter username"/>
+    <input onChange={changeEmail} name="email" placeholder="enter email"/>
+    <input onChange={changePassword} name="password" placeholder="enter password"/>
+    <button onClick={submit}>register</button>
+    </>
+)
+}
+export default Reg;
